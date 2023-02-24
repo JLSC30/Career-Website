1ea441999139b494c361f86d8755c5caf4fb5e99
Dangerous Pawn <38237419+JLSC30@users.noreply.github.com>
2023-02-24 11:28:50 +0000

    Add Dynamic data

--- app.py
+++ app.py
@@ -2,10 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS=[
+  {
+    'id': 1,
+    'title': 'Job 1',
+    'location': 'Job 1 location',
+    'salary': '1000$',
+  },{
+    'id': 2,
+    'title': 'Job 2',
+    'location': 'Job 2 location',
+    'salary': '2000$',
+  },{
+    'id': 3,
+    'title': 'Job 3',
+    'location': 'Job 3 location',
+  }
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
